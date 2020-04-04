dd52e704dc2bffce70f1aa11c64491ee92d95453
Russell Macalintal <russell.macalintal@gmail.com>
2020-04-04 20:12:27 +0000

    Automatically backed up by Learn

--- db/migrate/20200403055347_add_balance_to_users.rb
+++ db/migrate/20200403055347_add_balance_to_users.rb
@@ -1,5 +1,5 @@
 class AddBalanceToUsers < ActiveRecord::Migration[5.1]
   def change
-    add_column :users, :balance, :decimal, :default => 0.00, :precision =>7, :scale => 2
+    add_column :users, :balance, :decimal, :default => 0.00, :precision => 7, :scale => 2
   end
 end
